943612a473a90dfeae38766946e8fc9385acd63b
Patrick van Lohuizen <Patrick.van_Lohuizen@checkpt.com>
2017-08-15 15:42:55 +0200

    added test to compare different structures.

---
+++ Json.Comparer.Tests/CompareDifferentStructuresTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Dynamic;
+using System.Linq;
+using Json.Comparer.ValueConverters;
+using Xunit;
+
+namespace Json.Comparer.Tests
+{
+    public class CompareDifferentStructuresTests
+    {
+        [Fact]
+        public void Compare2EqualObjectsShouldNotHaveDifference()
+        {
+            dynamic simpleObject1 = new ExpandoObject();
+            simpleObject1.AAA = "";
+            dynamic simpleObject2 = new ExpandoObject();
+            simpleObject2.BBB = "";
+            var compareResult = new JTokenComparer(new IndexArrayKeySelector(), Enumerable.Empty<IComparrisonFilter>(), new ComparisonResult[] { ComparisonResult.MissingInSource2 }, new NonConvertingConverter()).Compare(simpleObject1, simpleObject2);
+
+            compareResult.ComparrisonResult.Should().Be(ComparisonResult.Identical, because: "The JObjects are created from the same CLR object instance");
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -1,3 +1,10 @@
 # Json.Comparer - Compare JTokens, JObjects, JArrays and 
 I have created Json.Comparer in order to allow for easy comparison of any object in .net and 
 comparing values from different systems and eventualy to run integrationtests and see if the results are as expected and if not to easily find any difference. The comparer is based upon newtonsoft.json.
+
+## Features
+- Compare json files and return difference.
+
+## Future
+- Return only difference as json object.
+- Revert object using compareresult.
